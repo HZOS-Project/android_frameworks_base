919109bdfcefd52ae3aed68f9c0bf1ed5a41ca77
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-29 15:44:41 +0800

    ApplicationPackageManager: Extend freeform window feature to all apps
    Change-Id: Iaa3e500fd6a80f4e1718218ff6d42fcb1eb79c9c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -875,6 +875,10 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.apps.photos.nexus_preload",
     };
 
+    private static final String[] featuresAndroid = {
+            "android.software.freeform_window_management"
+    };
+
     @Override
     public boolean hasSystemFeature(String name, int version) {
         String packageName = ActivityThread.currentPackageName();
@@ -894,6 +898,7 @@ public class ApplicationPackageManager extends PackageManager {
                 return false;
             }
         }
+        if (Arrays.asList(featuresAndroid).contains(name)) return true;
         if (Arrays.asList(featuresPixel).contains(name)) return true;
         if (Arrays.asList(featuresPixelOthers).contains(name)) return true;
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
